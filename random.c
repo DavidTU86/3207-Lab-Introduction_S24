b7214d3599bd0337adec5f8cb4234838d8513c3d
David Valadez <tur31231@temple.edu>
2024-01-19 15:07:04 -0500

    first

---
+++ random.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+
+char randchar() {
+    int rand_num = rand() % 25;
+    // Convert the random number to a character and return it
+    char random_char = 'A' + rand_num;
+    return random_char;
+}
